41a470175196119b6a815cc3fae729657bd7e6fa
Anton Gladky <gladky.anton@gmail.com>
2015-01-07 11:42:35 +0100

    Initial commit.

---
+++ lib/simobject.h
@@ -0,0 +1,30 @@
+/*
+    This file is part of libdem
+    libdem is the library, implementing discrete element method functionality.
+    Copyright (C) 2015 TU Bergakademie Freiberg, Institute for Mechanics and Fluid Dynamics
+
+    Author: 2015 Anton Gladky <gladky.anton@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libdem is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libdem.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include "base.h"
+
+class simobject {
+  private:
+
+  public:
+    simobject();
+};
